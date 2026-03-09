d760ca923bc2800875a0ea96de425b7a6ae85893
Yaboi Moose <a@b.c>
2026-03-09 01:55:07 +0000

    finally solved the longest line exercise

--- src/ch1/exercises/1-16-print_longest_line_v2.c
+++ src/ch1/exercises/1-16-print_longest_line_v2.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-#define CHUNK 1024	/* maximum input line size */
+#define MAXLINE 100	/* maximum input line size */
+#define OVERMAX 1	/* line is larger than MAXLINE */
+#define UNDERMAX 0	/* line is shorter than MAXLINE */
 
 int get_line(char line[], int maxline);
 void copy(char to[], char from[]);
@@ -9,26 +11,51 @@ int main()
 {
 	int len;		/* current line length */
 	int max;		/* maximum length seen so far */
-	char line[CHUNK];	/* current input line */
-	char longest[CHUNK];	/* longest line saved here */
+	int fcount;		/* count times line is full */
+	int part;		/* flag to track multipart lines */
+	char line[MAXLINE];	/* current input line */
+	char longest[MAXLINE];	/* longest line saved here */ 
 
 	max = 0;
-	while ((len = get_line(line)) > 0)
-		if (len > max) {
-			max = len;
-			copy(longest, line);
+	part = UNDERMAX;
+	// while there is another line
+	while ((len = get_line(line, MAXLINE)) > 0) {
+		if (part == UNDERMAX)
+			fcount = 0;
+		// if it's the max length and last char is not newline
+		if (len == MAXLINE - 1 && line[MAXLINE-2] != '\n') {
+			// save the first part of the line
+			if (part == UNDERMAX)
+				copy(longest, line);
+			// increment full count 
+			++fcount;
+			part = OVERMAX;
+			// and check next section
+			continue;
 		}
+		len += (MAXLINE - 1) * fcount;
+		// if it's longer than the previous longest
+		if (len > max) {
+			// save the length
+			max = len;
+			// save the line if not an overmax line
+			if (part == UNDERMAX)
+				copy(longest, line);
+			part = UNDERMAX;
+		}
+	}
 	if (max > 0)		/* there was a line */
-		printf("%s", longest);
+		printf("len: %d; line:%s\n", max, longest);
 	return 0;
 }
 
 /* get_line: read a line into s, return length */
-int get_line(char s[])
+int get_line(char s[], int lim)
 {
 	int c, i;
 
-	for (i = 0; (c = getchar()) != EOF &&
+	for (i = 0; i < lim-1 &&
+	     (c = getchar()) != EOF &&
 	     c != '\n'; ++i)
 		s[i] = c;
 	if (c == '\n') {
