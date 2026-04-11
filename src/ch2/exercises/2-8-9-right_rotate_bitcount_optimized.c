f87de0635d55e52ae27a0c2604adfce5050f322e
Yaboi Moose <a@b.c>
2026-04-11 11:52:37 +0000

    ex 2-9 added to 2-8 solution; testing files

--- src/ch2/exercises/2-8-9-right_rotate_bitcount_optimized.c
+++ src/ch2/exercises/2-8-9-right_rotate_bitcount_optimized.c
@@ -5,6 +5,7 @@
 /* prototypes */
 int rightrot(int x, int n);
 int bitcount(unsigned int);
+int bitcount_optimized(unsigned int);
 
 int main(void)
 {
@@ -22,8 +23,8 @@ int rightrot(int x, int n)
 {
 	int m_x = ~(~0 << n);			// bit mask for x
 	int b = x & m_x;			// buffer to store bits before rotate
-	int z = bitcount(~0);			// int size
-	return (x >> n) | (b << ( - n));	// rotate and restore
+	int z = bitcount_optimized(~0);		// int size
+	return (x >> n) | (b << z - n);		// rotate and restore
 }
 
 /* bitcount: count 1 bits in x */
@@ -35,4 +36,16 @@ int bitcount(unsigned int x)
 		if (x & 01)
 			b++;
 	return b;
+}
+
+/* bitcount_optimized: count 1 bits in x */
+int bitcount_optimized(unsigned int x)
+{
+	if (x == 0)
+		return 0;
+
+	int b = 0;
+	while (x &= (x - 1))
+		b++;
+	return ++b;
 }
