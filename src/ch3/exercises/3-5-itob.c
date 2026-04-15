a16e4a443753a095af8ebcc5f5020cd006ba973a
Yaboi Moose <a@b.c>
2026-04-15 21:49:39 +0000

    add 3-5

--- src/ch3/exercises/3-5-itob.c
+++ src/ch3/exercises/3-5-itob.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <limits.h>
 
 /* k&r exercise 3-5 */
+
+/* macros */
+
+/* prototypes */
+void itob(int, char [], int);
+void reverse(char []);
 
 /*
  * Write the function `itob(n, s, b)` that converts the integer
@@ -8,18 +16,49 @@
  * In particular, `itob(n, s, 16)` formats n as a hexidecimal
  * integer in s.
  */
-
-/* prototypes */
-void itob(int, char [], int);
-
 int main(int argc, char *argv[])
 {
+	int i = 19;
+	char s[16];
+
+	itob(i, s, 2);
+	printf("i is %i\ns is %s\n", i, s);
 	return 0;
 }
 
 /* itob: converts integer n into a base char representation
  * in the string s. */
-void itoa(int n, char s[], int b)
+void itob(int n, char s[], int b)
 {
+	int i, t;
+	int sign = 1;			/* assume positive */
 
+	if (n < 0)
+		sign = -sign;		/* flip sign */
+
+	i = 0;
+	do {
+		t = (n % b * sign);	/* get next digit */
+		/*  use next digit to determine add '0' or 'a' */
+		if (t >= 10)
+			t += 'a' - 10;
+		else
+			t += '0';
+
+		s[i++] = t;		/* save */
+	} while (n /= b);		/* delete it */
+	if (sign < 0)
+		s[i++] = '-';
+	s[i] = '\0';
+	reverse(s);
 }
+
+/* reverse:  reverse string s in place */
+void reverse(char s[])
+{
+	int c, i, j;
+
+	for (i = 0, j = strlen(s)-1; i < j; i++, j--)
+		c = s[i], s[i] = s[j], s[j] = c;
+}
+
