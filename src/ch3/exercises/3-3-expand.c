2a906175289e7882d7fe73d8e741f6e001fe28a0
Yaboi Moose <a@b.c>
2026-04-15 18:07:28 +0000

    finish 3-3

--- src/ch3/exercises/3-3-expand.c
+++ src/ch3/exercises/3-3-expand.c
@@ -21,7 +21,7 @@ int fill(char [], int, int, int);
  */
 int main(int argc, char *argv[])
 {
-	char s1[] = "test1 a-z\n\ttest2 A-Z\n\ttest3 0-9\n\ttest4 a-l0-5\n\ttest5 -a-f";
+	char s1[] = "test1 a-z\n\ttest2 A-Z\n\ttest3 0-9\n\ttest4 a-l0-5\n\ttest5 -a-f\n\ttest6 q-c\n\ttest7 A-F-0";
 	char s2[ARR_SZ];
 
 	expand(s1, s2);
@@ -39,38 +39,36 @@ void expand(char s1[], char s2[])
 {
 	int i, j;
 	/* loop through s1 checking each char */
-	for (i = j = 0; i < strlen(s1) - 1; ++i, ++j) {
+	for (i = j = 0; i < strlen(s1); ++i) {
 		if (s1[i] == '-' && ( /* '-' found */
 			(islower(s1[i-1]) && islower(s1[i+1])) || /* both lower */ 
 			(isupper(s1[i-1]) && isupper(s1[i+1])) || /* both upper */
 			(isdigit(s1[i-1]) && isdigit(s1[i+1]))	/* both digit */
 		)) {
-			/* copy chars into s2 between char before and after '-' */
-			j = fill(s2, --j, s1[i-1], s1[i+1]);
+			/* replace hyphen with chars it represents and fast-forward j */
+			j = fill(s2, j, s1[i-1], s1[i+1]);
 		} else {
-			s2[j] = s1[i];
+			s2[j++] = s1[i];
 		}
 	}
 	s2[j] = '\0';
 }
 
-/* fill: fills s from index si with contiguous ASCII characters between f and l
+/* fill: fills s from index si with contiguous ASCII characters between
+ * f and l, non-inclusive
  * returns current index si completed on */
 int fill(char s[], int si, int f, int l)
 {
 	int i;
 
-	printf("\nf is %c = %i; l is %c = %i\n", f, f, l, l);
-
-	if (f == l) {
-		s[si++] = f;
-		s[si++] = l;
-	} else if (f < l) {
-		for (i = f; i <= l; ++i)
+	if (f == l)
+		return si;
+	if (f < l)
+		for (i = f+1; i < l; ++i)
 			s[si++] = i;
-	} else {
-		for (i = f; i >= l; --i)
+	else
+		for (i = f-1; i > l; --i)
 			s[si++] = i;
-	}
+	
 	return si;
 }
