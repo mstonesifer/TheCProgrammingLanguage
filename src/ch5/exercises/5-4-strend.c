92cf8a3c0a051ae37e6dea1a5cf7485f174950af
Yaboi Moose <a@b.c>
2026-04-28 03:58:16 +0000

    add ex 3 and 4

--- src/ch5/exercises/5-4-strend.c
+++ src/ch5/exercises/5-4-strend.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 /* k&r exercise 5-4 */
 
@@ -11,15 +12,35 @@ int strend(char *, char *);
 /*
  * Exercise description:
  * Write the function strend(s, t) which returns 1
- * if the string t occurs at th end of the string
+ * if the string t occurs at the end of the string
  * s, and zero otherwise.
  */
 int main(int argc, char *argv[])
 {
+	int result;
+	char first[] = "Hello, world!";
+	char test[] = "orld!";
+	char test2[] = "Hello";
+
+	result = strend(first, test);
+	printf("first result: %i\n", result);
+	result = strend(first, test2);
+	printf("second result: %i\n", result);
+
 	return 0;
 }
 
 /* functions */
 int strend(char *s, char *t)
 {
+
+	int tlen = strlen(t);	
+
+	while (*s++)	/* find end of s */
+		;
+	s -= tlen;	/* go back length of t */
+	while (*t++)
+		if (*s++ != *t) /* any from here don't match it's different */
+			return 0;
+	return 1;
 }
