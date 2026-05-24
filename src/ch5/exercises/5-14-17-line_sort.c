04d3c7013761bcc165fee2b4b181898295ac602e
Yaboi Moose <a@b.c>
2026-05-24 20:20:00 +0000

    sorting working asc/desc alpha/numeric

--- src/ch5/exercises/5-14-17-line_sort.c
+++ src/ch5/exercises/5-14-17-line_sort.c
@@ -3,10 +3,11 @@
 #define ASC 1
 #define DESC 0
 
-#define F_NUMERIC 1		/* sort by numeric val */
-#define F_REVERSE 1 << 1	/* sort in reverse order */
-#define F_FOLD 1 << 2		/* ignore case */
-#define F_DIR 1 << 3		/* only compare letters, nums, blanks */
+#define F_DEBUG 1		/* debug mode */
+#define F_NUMERIC 1 << 1	/* sort by numeric val */
+#define F_REVERSE 1 << 2	/* sort in reverse order */
+#define F_FOLD 1 << 3		/* ignore case */
+#define F_DIR 1 << 4		/* only compare letters, nums, blanks */
 
 #define NUL '\0'
 #define NEWLINE '\n'
@@ -16,43 +17,42 @@
 #define CVAL(x) (x - '0')	/* numeric value of char x */
 #define TEN_X_F(x) (x * 10.0)	/* multiply x by ten and convert to float */
 
+static int flags = 1;		/* set to 1 for debug mode */
+
 int readargs(int, char **);
 int xreadlines(char **s);
 int xgetline(char *, int);
 int xstrlen(char *);
 void xstrcpy(char *, char *);
 char *cache_alloc(int);
-void xwritelines(char **s);
-void xqsort(void **, int, int, int (*)(void *, void *));
+void printresult(char **s);
+void xqsort(void **, int, int, int, int (*)(void *, void *));
 double xatof(char *);
 int xstrcmp(char *, char *);
 int numcmp(char *, char *);
 void xswap(void **, int, int);
+void wdbg(char *);
 
 /* line_sort:  sort input lines by various criteria */
 int main(int argc, char *argv[])
 {
 	int lc;
-	int flags = 0;			/* keep track of switches */
 	char *lines[MAX_LINE_COUNT];	/* line collection */
 
 	// read clargs
 	flags = readargs(argc, argv);
-	if (flags & F_NUMERIC)
-		printf("numeric\n");
-	if (flags & F_REVERSE)
-		printf("reverse\n");
-
+	
 	// read in lines
 	lc = xreadlines(lines);
 
-	printf("%i lines read\n", lc);
+	printf("line_sort: sorting %i lines...", lc);
 	// sort by flags
-	xqsort((void **) lines, 0, lc, 
-			(int (*)(void *, void *))((flags & F_NUMERIC) ? numcmp : xstrcmp)); 
+	xqsort((void**) lines, 0, lc-1, (flags & F_REVERSE), 
+			(int (*)(void*, void*))((flags & F_NUMERIC) ? numcmp : xstrcmp)); 
+	printf("done!\n");
 
 	// write sorted lines
-	xwritelines(lines);
+	printresult(lines);
 
 	return 0;
 }
@@ -61,7 +61,6 @@ int main(int argc, char *argv[])
 int readargs(int count, char **args)
 {
 	int c;
-	int flags = 0;
 
 	while (--count > 0 && *(++args)[0] == '-') {
 		while (NUL != (c = *++args[0]))
@@ -150,41 +149,80 @@ char *cache_alloc(int n)
 	return cachep - n;
 }
 
-/* writelines:  writes all lines stored in memory cache */
-void xwritelines(char **s)
+/* printresult:  writes all lines stored in memory cache */
+void printresult(char **s)
 {
+	printf("\n\n");
+	printf("=== SORTED ");
+	
+	if (flags & F_NUMERIC)
+		printf("NUMERIC ");
+		
+	printf("INPUT IN ");
+
+	if (flags & F_REVERSE)
+		printf("DESCENDING ");
+	else
+		printf("ASCENDING ");
+
+	printf("ORDER ===\n");
+	
 	while (NULL != *s && xstrlen(*s) > 0)
 		printf("%s", *s++);
 }
 
 
 /* qsort:  sort v using parameter comparator routine */
-void xqsort(void *v[], int left, int right, int (*comp)(void *, void *))
+void xqsort(void *v[], int left, int right, int isr, int (*comp)(void *, void *))
 {
-	putchar('-');
+	wdbg("sorting");
 	int i, last;
 
 	if (left >= right)	/* do nothing if array contains */
 		return;		/* fewer than two elements */
+	wdbg("swapping left with mid");
 	xswap(v, left, (left + right)/2);
 	last = left;
-	for (i = left+1; i <= right; i++)
-		if ((*comp)(v[i], v[left]) < 0)
-			xswap(v, ++last, i);
+
+		
+	for (i = left+1; i <= right; i++) {
+		for (int z = 0; z < 5; z++) {
+			printf("%s", (char*)v[z]);
+		}
+		printf("\n");
+		//wdbg("checking against mid");
+		printf("left = %s\n", (char*)v[left]);
+		if (isr) {
+		       if ((*comp)(v[i], v[left]) >= 0) 
+				xswap(v, ++last, i);
+		} else {
+		       	if ((*comp)(v[i], v[left]) < 0)
+				xswap(v, ++last, i);
+		}
+	}
+	wdbg("swapping left with last");
 	xswap(v, left, last);
-	xqsort(v, left, last-1, comp);
-	xqsort(v, last+1, right, comp);
-	printf("done!");
+	wdbg("calling qsort for left side");
+	xqsort(v, left, last-1, isr, comp);
+	wdbg("calling qsort for right side");
+	xqsort(v, last+1, right, isr, comp);
+	wdbg("done!");
 }
 
 /* strcmp:  compare two strings, asciibetically */
 int xstrcmp(char *s, char *t)
 {
+	wdbg("compare strings");
 	while (*s == *t) {
-		if (NUL == *s)
+		wdbg("is s = t?");
+		if (NUL == *s) {
+			wdbg("yes!");
 			return 0;
+		}
 		s++, t++;
 	}
+
+	wdbg("no.");
 
 	return *s - *t;
 }
@@ -192,22 +230,31 @@ int xstrcmp(char *s, char *t)
 /* numcmp:  compare two strings, numerically */
 int numcmp(char *s, char *t)
 {
+	wdbg("compare nums");
 	double u, v;
 
 	u = xatof(s);
 	v = xatof(t);
 
-	if (u > v)
+	printf("[DBG]: is %f > %f?\n", u, v);
+	if (u > v) {
+		wdbg("yes");
 		return 1;
-	if (u < v)
+	}
+	if (u < v) {
+		wdbg("no");
 		return -1;
+	}
 
+	wdbg("they are eq");
 	return 0;
 }
 
 /* swap:  switch the contents of an array at indecies i and j in place */
 void xswap(void *v[], int i, int j)
 {
+	//wdbg("swapping");
+	printf("swapping %s with %s\n", (char*)v[i], (char*)v[j]);
 	void *tmp;
 
 	tmp = v[i];
@@ -236,7 +283,7 @@ double xatof(char *s)
 	while (xisdigit(c = *s++))
 		n = TEN_X_F(n) + CVAL(c);
 
-	printf("n = %f\n", n);
+	/* done if next non-digit is not point */
 	if ('.' != c)
 		return n;
 
@@ -245,7 +292,6 @@ double xatof(char *s)
 	while (xisdigit(c = *s++)) {
 		n = TEN_X_F(n) + CVAL(c);
 		pow = TEN_X_F(pow);
-		printf("n = %f | pow = %f\n", n, pow);
 	}
 
 	return sign * n / pow;
@@ -260,3 +306,10 @@ int xisspace(char c)
 {
 	return c == ' ';
 }
+
+void wdbg(char *s)
+{
+	if (flags & F_DEBUG)
+		printf("[DBG]: %s\n", s);
+}
+
